f46140a9aad8cbcfd1570ede8b5665320e93db10
FTH-Bozkurt <apsarifatih2004@gmail.com>
2025-03-23 22:21:18 +0300

    use of NameSpace

---
+++ Namespace Kullanımı ve Genişletilebilirlik/Namespace Kullanımı ve Genişletilebilirlik/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MatematikIslemleri
+{
+    public class Hesaplama
+    {
+
+        
+        public double Toplama(double sayı1, double sayı2)
+        {
+            return sayı1 + sayı2;
+        }
+
+        public double Carpma(double sayı1, double sayı2)
+        {
+            return sayı1 * sayı2;
+        }
+    }
+}
+
+
+namespace Uygulama
+
+{
+    using MatematikIslemleri;
+
+    class program
+    {
+        static void Main()
+        {
+            Hesaplama hesaplama = new Hesaplama();
+            double ToplamaSonucu = hesaplama.Toplama(12.5, 6.4);
+            double CarpmaSonucu = hesaplama.Carpma(12.5, 6.4);
+
+            Console.WriteLine("Toplam sonuç: " + ToplamaSonucu);
+            Console.WriteLine("carpma sonucu: " + CarpmaSonucu);
+        }
+
+    }
+
+
+}
+
+
+
