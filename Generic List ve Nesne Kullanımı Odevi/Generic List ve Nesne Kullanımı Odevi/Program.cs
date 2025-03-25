ac3827609c651a806167e338825ed80468b5f4b6
FTH-Bozkurt <apsarifatih2004@gmail.com>
2025-03-25 16:56:54 +0300

    Generic List ve Nesne Kullanımı Ödevi

---
+++ Generic List ve Nesne Kullanımı Odevi/Generic List ve Nesne Kullanımı Odevi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Car
+{
+    public string Marka { get; set; }
+    public string Model { get; set; }
+    public double FuelConsumption { get; set; }
+    public double TotalDistance { get; set; }
+    public double TotalFuelConsumption()
+    {
+        return (TotalDistance / 100) * FuelConsumption;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Car> arabalar = new List<Car>
+        {
+            new Car { Marka = " Toyota ", Model = " Corolla ", FuelConsumption = 6.5, TotalDistance = 5000 },
+            new Car { Marka = "BMW", Model = "X5", FuelConsumption = 9.0, TotalDistance = 5000 },
+            new Car { Marka = "Mercedes", Model = "C200", FuelConsumption = 8.2, TotalDistance = 5000 }
+        };
+
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine("Marka: " + araba.Marka + 
+                              " Model: " + araba.Model+
+                              " 100 km'de Yaktığı Yakıt: " +araba.FuelConsumption + " litre, Toplam Yakıt Tüketimi: " + araba.TotalFuelConsumption()+ " litre");
+        }
+    }
+}
